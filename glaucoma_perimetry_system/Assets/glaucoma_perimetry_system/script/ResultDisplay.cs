619a473581b7c0046faa4ed0fd0cbb2404b6dc23
lichengyu <lichengyu@sensorsdata.cn>
2021-03-13 01:01:44 +0800

    feat:明天开始处理后的图片储存，灰度图处理暂时放在之后，先走完整个流程

--- glaucoma_perimetry_system/Assets/glaucoma_perimetry_system/script/ResultDisplay.cs
+++ glaucoma_perimetry_system/Assets/glaucoma_perimetry_system/script/ResultDisplay.cs
@@ -5,43 +5,71 @@ using UnityEngine.XR;
 public class ResultDisplay : MonoBehaviour
 {
     public Material material;
-
-    private void Start()
+    //定义视标显示状态数组(左眼)
+    bool[] sightingPostDisplayStatus = new bool[72]
     {
-        OnPostRender();
+        false,false,true,true,true,true,false,false,false,
+        false,true,true,true,true,true,true,false,false,
+        true,true,true,true,true,true,true,true,false,
+        true,true,true,true,true,true,true,true,true,
+        true,true,true,true,true,true,true,true,true,
+        true,true,true,true,true,true,true,true,false,
+        false,true,true,true,true,true,true,false,false,
+        false,false,true,true,true,true,false,false,false
+    };
+    //定义视标显示状态数组(右眼)
+   // bool[] sightingPostDisplayStatus = new bool[72]
+   //{
+   //     false,false,false,true,true,true,true,false,false,
+   //     false,false,true,true,true,true,true,true,false,
+   //     false,true,true,true,true,true,true,true,true,
+   //     true,true,true,true,true,true,true,true,true,
+   //     true,true,true,true,true,true,true,true,true,
+   //     false,true,true,true,true,true,true,true,true,
+   //     false,false,true,true,true,true,true,true,false,
+   //     false,false,false,true,true,true,true,false,false
+   //};
+    void Start()
+    {
+        
     }
     // Update is called once per frame
     void Update()
     {
-        
+        OnPostRender();
     }
     void OnPostRender()
     {
-        //if (SightingPostLocationDeal.count == 72)
-        //{
+        if (SightingPostLocationDeal.count == 72)
+        {
             //XRSettings.enabled = false;
             GL.Clear(true, true, Color.black);
             GL.PushMatrix();
-            GL.LoadOrtho();
+            //GL.LoadOrtho();
             material.SetPass(0);
             GL.Begin(GL.QUADS);
             GL.Color(Color.white);
-            for (var k = 0; k < SightingPostLocationDeal.sightingPostLocation.Rank; k++)
+            for (var k = 0; k < SightingPostLocationDeal.sightingPostLocation.GetLength(0); k++)
             {
+                if (sightingPostDisplayStatus[k]==true) {
+                    GL.Vertex3(SightingPostLocationDeal.sightingPostLocation[k, 0] - 0.5f, SightingPostLocationDeal.sightingPostLocation[k, 1] - 0.5f, 0);
 
-                GL.Vertex3(SightingPostLocationDeal.sightingPostLocation[k,0]/10 + 0.5f, SightingPostLocationDeal.sightingPostLocation[k,1]/10+ 0.5f, 0);
+                    GL.Vertex3(SightingPostLocationDeal.sightingPostLocation[k, 0] + 0.5f, SightingPostLocationDeal.sightingPostLocation[k, 1] - 0.5f, 0);
 
-                GL.Vertex3(SightingPostLocationDeal.sightingPostLocation[k, 0]/10 + 0.51f, SightingPostLocationDeal.sightingPostLocation[k, 1]/10 + 0.5f, 0);
+                    GL.Vertex3(SightingPostLocationDeal.sightingPostLocation[k, 0] + 0.5f, SightingPostLocationDeal.sightingPostLocation[k, 1] + 0.5f, 0);
 
-                GL.Vertex3(SightingPostLocationDeal.sightingPostLocation[k, 0]/10 + 0.51f, SightingPostLocationDeal.sightingPostLocation[k, 1]/10 + 0.51f, 0);
-
-                GL.Vertex3(SightingPostLocationDeal.sightingPostLocation[k, 0]/10 + 0.5f, SightingPostLocationDeal.sightingPostLocation[k, 1]/10 + 0.51f, 0);
+                    GL.Vertex3(SightingPostLocationDeal.sightingPostLocation[k, 0] - 0.5f, SightingPostLocationDeal.sightingPostLocation[k, 1] + 0.5f, 0);
+                }
+                else
+                {
+                    continue;
+                }
 
             }
             GL.End();
             GL.PopMatrix();
 
 
-        //}
+        }
     }
 }
